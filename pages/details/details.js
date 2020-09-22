347fb1f4985b389fe23c41748c175e4490cb1d92
korealu <643949593@qq.com>
2020-09-22 11:29:08 +0800

    1,1-beta-static

--- pages/details/details.js
+++ pages/details/details.js
@@ -1,4 +1,6 @@
 // pages/details/details.js
+
+import polyv from '../../utils/polyv';
 Page({
 
   /**
@@ -6,9 +8,32 @@ Page({
    */
   data: {
     // 显示咨询或者聊天
-    showInfoModal: true
+    showInfoModal: true,
+    // 视频链接
+    videoSrc: '',
+    // 视频时长
+    duration: '',
+    // 初始化播放时间
+    initialTime: ''
   },
 
+  onReady() {
+    const that = this;
+     //获取视频播放地址,根据后台返回的开播时间，取服务器时间跟开播时间的时间差
+     let vid = "88083abbf5bcf1356e05d39666be527a_8";
+     let vidObj = {
+         vid,
+         callback(videoInfo) {
+           console.log(videoInfo);
+             that.setData({
+               videoSrc: videoInfo.src[0],
+               duration: videoInfo.duration,
+               initialTime: videoInfo.duration - 100
+             });
+         }
+     };
+     polyv.getVideo(vidObj);
+  },
 
   setInfoModal(e) {
     console.log(e)
